166d3dc08e58dd4b9a337e96d2baca2b6564a322
karavanets <dront22rus@mail.ru>
2022-10-29 14:06:21 +0700

    add def get_data(url) with headers > user-agent

--- scraper.py
+++ scraper.py
@@ -2,3 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
+def get_data(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    }
