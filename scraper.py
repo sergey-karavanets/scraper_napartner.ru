9eaa6522b1756f189b465c65cc37d61045996963
karavanets <dront22rus@mail.ru>
2022-10-29 14:24:24 +0700

    comment out variable 'req', comment out creater file 'project.html'

--- scraper.py
+++ scraper.py
@@ -7,10 +7,10 @@ def get_data(url):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
     }
 
-    req = requests.get(url, headers)
-
-    with open('project.html', 'w', encoding='utf-8') as file:
-        file.write(req.text)
+    # req = requests.get(url, headers)
+    #
+    # with open('project.html', 'w', encoding='utf-8') as file:
+    #     file.write(req.text)
 
     with open('project.html', encoding='utf-8') as file:
         src = file.read()
