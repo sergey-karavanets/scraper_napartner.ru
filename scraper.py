96da68711afd5eece98e4c17d68b997faa0d5810
karavanets <dront22rus@mail.ru>
2022-10-29 14:10:53 +0700

    add variable req

--- scraper.py
+++ scraper.py
@@ -6,3 +6,5 @@ def get_data(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
     }
+
+    req = requests.get(url, headers)
